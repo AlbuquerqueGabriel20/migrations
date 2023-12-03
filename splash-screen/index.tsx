57dc1d3c6de33d124f08e0a07e48fe2ae9e1d429
Gabriel Lucena <82548571+AlbuquerqueGabriel20@users.noreply.github.com>
2023-12-02 22:17:21 -0300

    Update and rename index.jsx to index.tsx

--- splash-screen/index.tsx
+++ splash-screen/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./style.css";
 
-export const SplashScreen = () => {
+export const SplashScreen = (): JSX.Element => {
   return (
     <div className="splash-screen">
       <img className="logo" alt="Logo" src="logo.png" />
     </div>
   );
-};// test page
+};
