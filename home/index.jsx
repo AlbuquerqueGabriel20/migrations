812425c7ae45b23f40320ee3bc4a4e6af6afb77f
Gabriel Lucena <82548571+AlbuquerqueGabriel20@users.noreply.github.com>
2023-12-02 22:07:53 -0300

    Update index.jsx

--- home/index.jsx
+++ home/index.jsx
@@ -1,1 +1,25 @@
+import React from "react";
+import "./style.css";
+
+export const Home = (): JSX.Element => {
+  return (
+    <div className="home">
+      <div className="div">
+        <div className="overlap-group">
+          <div className="text-wrapper">Escaneie o QR Code</div>
+          <div className="overlap">
+            <img className="q-rcode" alt="Q rcode" src="q-rcode.png" />
+            <img className="chevron-left" alt="Chevron left" src="chevron-left.svg" />
+            <img className="img" alt="Chevron left" src="chevron-left-2.svg" />
+            <img className="chevron-left-2" alt="Chevron left" src="chevron-left-3.svg" />
+            <img className="chevron-left-3" alt="Chevron left" src="image.svg" />
+          </div>
+          <div className="text-wrapper-2">24/11/2023 - 19h00</div>
+          <p className="p">Rua Borges Figueiredo, Mooca - SP</p>
+        </div>
+        <img className="align-justify" alt="Align justify" src="align-justify.svg" />
+      </div>
+    </div>
+  );
+};
 // test page
